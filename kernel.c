30d59409082bead3da904ef4799ee7d9012618c2
Edward Miller <edwardomiller123@gmail.com>
2023-07-14 20:34:26 +0100

    change entry back to main

--- kernel.c
+++ kernel.c
@@ -1,4 +1,4 @@
-void start()
+void main()
 {
   // Create a pointer to a char and point it to a text cell in video memory.
   char *character = (char *) 0xb8000 + 2400;
--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,5 +1,5 @@
 ; define the entry point for our kernel
 [bits 32]
-[extern _start]
-call _start
+[extern main]
+call main
 jmp $
