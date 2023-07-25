5253fd235d6578e67416a1ac8e4b97c802ae704a
Edward Miller <edwardomiller123@gmail.com>
2023-07-25 21:05:32 +0100

    ps2 test

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,7 +5,5 @@ void main()
   clear_screen();
   print_string_at("edOS.v1.0\n", 0, 1);
   testControllers();
-  // int ps2response = initPS2Controller();
-  // print_string(intToString(ps2response));
-  print_string(intToString(16));
+  print_string(intToString(162343));
 }
