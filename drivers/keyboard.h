149326e9d81cf1d0492535a5d4d863196aa85b97
Edward Miller <edwardomiller123@gmail.com>
2023-09-06 19:59:44 +0100

    move function headers

--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -17,13 +17,13 @@ void testPort1();
 
 void testPort2();
 
+// testPS2Controller runs a simple test to see if the ps2 controller is working.
+void testPS2Controller();
+
 // applyKeyPress prints the corresponding character for a given key code.
 void applyKeyPress(int keyCode);
 
 void handleKeyInput(struct registers r);
-
-// testPS2Controller runs a simple test to see if the ps2 controller is working.
-void testPS2Controller();
 
 void initPS2Keyboard();
 
