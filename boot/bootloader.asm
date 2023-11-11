a727b8c8e4b68534bda9524a11fb0515eb362d09
edwardMiller123 <edwardomiller123@gmail.com>
2023-11-11 15:17:48 +0000

    update comments

--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -79,12 +79,12 @@ BOOT_DRIVE db 0
 ; nasm interprets 13 as carriage return and 10 as line feed
 ; so this is equivalent to printing '\n message'
 ; we dont add this for the 32 bit string as we are directly
-; accessing video memory
+; accessing video memory rather than using the bios.
 MSG_REAL_MODE db 13,10,"Booted in 16-bit real mode", 0
 MSG_PROTECTED_MODE db "Booted into 32-bit protected mode", 0
 MSG_LOAD_KERNEL db 13,10,"Loading kernel", 0
 
-; magic number
+; magic number. Where the magic begins.
 times 510-($-$$) db 0
 dw 0xaa55
 
